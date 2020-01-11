d8ca316a186f6f1cad0932ace97939d4d2894edb
simonlacey20 <simonlacey20@gmail.com>
2020-01-11 14:15:19 -0500

    s7 lsn39 done

--- public/script.js
+++ public/script.js
@@ -3,14 +3,59 @@ new Vue({
 	data: {
 		total: 0,
 		products: [
-			{ title: "Product 1" },
-			{ title: "Product 2" },
-			{ title: "Product 3" },
-		]
+			{ title: "Product 1", id: 1, price: 9.99 },
+			{ title: "Product 2", id: 2, price: 9.99 },
+			{ title: "Product 3", id: 3, price: 9.99 }
+		],
+		cart: [],
+		search: ""
 	},
 	methods: {
-		addToCart: function() {
-			this.total += 9.99;
+		addToCart: function(product) {
+			this.total += product.price;
+			var found = false;
+			for (var i = 0; i < this.cart.length; i++) {
+				if (this.cart[i].id === product.id) {
+					this.cart[i].qty++;
+					found = true;
+				}
+			}
+			if (!found) {
+				this.cart.push({
+					id: product.id,
+					title: product.title,
+					price: product.price,
+					qty: 1
+				});
+			}
+		},
+		inc: function(item) {
+			item.qty++;
+			this.total += item.price;
+		},
+		dec: function(item) {
+			item.qty--;
+			this.total -= item.price;
+			if (item.qty <= 0) {
+				var i = this.cart.indexOf(item);
+				// this.cart.indexOf(item);
+				this.cart.splice(i, 1);
+			}
+		},
+		onSubmit: function() {
+			// console.log("Search");
+			var path = "/search?q=".concat(this.search);
+			this.$http.get(path)
+				.then(function(response) {
+					// console.log(response);
+					this.products = response.body;
+				});
+		}
+	},
+
+	filters: {
+		currency: function(price) {
+			return "$".concat(price.toFixed(2));
 		}
 	}
 });
